8b6a6bdda4da37eeb5e37d8359386ced21f96453
Matvey Kulikov <51297218+desup6@users.noreply.github.com>
2025-01-22 11:30:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+import asyncio
+import logging
+
+from media_collector import MediaCollector
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    # filename="app.log",
+                    # filemode="a")
+                    )
+logger = logging.getLogger(__name__)
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+    SESSION_NAME = config["session_name"]
+    API_ID = config["api_id"]
+    API_HASH = config["api_hash"]
+    TARGET_CHANNEL = config["target_channel"]
+    SOURCES = config["sources"]
+    CHANCE = config["chance"]
+    BOTTOM_DELAY = config["bottom_delay"]
+    TOP_DELAY = config["top_delay"]
+    WATERMARK_PATH = config["watermark_path"]
+    CAPTION_TEXT = config["caption_text"]
+
+
+async def main():
+    collector = MediaCollector(SESSION_NAME,
+                               API_ID,
+                               API_HASH,
+                               TARGET_CHANNEL,
+                               SOURCES,
+                               CHANCE,
+                               BOTTOM_DELAY,
+                               TOP_DELAY,
+                               CAPTION_TEXT)
+    await collector.init_client()
+    await collector.client.run_until_disconnected()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.info("Бот остановлен вручную")
+    except Exception as e:
+        logging.error(f"Критическая ошибка: {str(e)}")
